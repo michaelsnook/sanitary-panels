a7b398ea53281458695a9cf494da0019ad0132b5
Michael Snook <michael.snook@gmail.com>
2016-07-10 21:05:47 +0530

    move function to functions.php

--- wp-content/themes/sanitary-panels/functions.php
+++ wp-content/themes/sanitary-panels/functions.php
@@ -25,4 +25,27 @@ foreach ($sage_includes as $file) {
 
   require_once $filepath;
 }
+
 unset($file, $filepath);
+
+// post_to_anchor_or_hash (function)
+// ---------------------------------
+// added by @michaelsnook to take a post, and either print its link
+// or print a disabled link to "#".
+// * $classnames: add classnames e.g. 'next-post comic navlink'
+// * $rel: add rel attribute with e.g. 'prev'
+
+function post_to_anchor_or_hash($p, $classnames = '', $rel = '', $text= '') {
+  $payload_to_echo = '';
+  if ($classnames) $payload_to_echo .= '<a class="' . $classnames . '"';
+  if ($p) {
+    $payload_to_echo .= ' href="' . get_permalink($p) . '"';
+    if ($rel) $payload_to_echo .= ' rel="' . $rel . '"';
+    $payload_to_echo .= '>';
+  } else {
+    $payload_to_echo .= ' href="#" disabled>';
+  }
+  if ($text) $payload_to_echo .= $text;
+  $payload_to_echo .= '</a>';
+  echo $payload_to_echo;
+}
--- wp-content/themes/sanitary-panels/templates/comic-nav.php
+++ wp-content/themes/sanitary-panels/templates/comic-nav.php
@@ -1,24 +1,3 @@
-<?php
-
-// added by @michaelsnook to take a post, and either print its link
-// or print a disabled link to "#".
-// add classnames with strings like 'next-post comic navlink'
-// add rel with values like 'prev'
-
-function post_to_anchor_or_hash($p, $classnames = '', $rel = '') {
-  $payload_to_echo = '';
-  if ($p) {
-    $payload_to_echo .= '<a class="' . $classnames . '"';
-    $payload_to_echo .= ' href="' . get_permalink($p) . '"';
-    if ($rel) $payload_to_echo .= ' rel="' . $rel . '"';
-    $payload_to_echo .= '>previous</a>';
-  } else {
-    $payload_to_echo .= '<a class="'. $classnames .'" href="#" disabled></a>';
-  }
-  echo $payload_to_echo;
-}
-?>
-
 <div class="col-sm-12 text-center">
 <nav class="sprites">
 
@@ -27,11 +6,13 @@ function post_to_anchor_or_hash($p, $classnames = '', $rel = '') {
     $p = get_previous_post(true);
     post_to_anchor_or_hash( $p, "sprite-prev", "prev" );
   ?>
-  <a class="sprite-random" href="<?php echo get_bloginfo('wpurl'); ?>/random/">random</a>
+
+  <a class="sprite-random" href="<?php echo get_bloginfo('wpurl'); ?>/random/" rel="random">random</a>
   <?php
     $p = get_next_post(true);
     post_to_anchor_or_hash( $p, "sprite-next", "next" );
   ?>
+
   <a class="sprite-last" href="<?php echo get_bloginfo('wpurl'); ?>/last/">last</a>
 
 </nav>
