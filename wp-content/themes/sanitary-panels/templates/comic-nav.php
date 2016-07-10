e6c86d09bf7b98656602819b8bc84f0fedb17aeb
Michael Snook <michael.snook@gmail.com>
2016-07-10 17:11:22 +0530

    makes the comic nav work with prev and next in category

--- wp-content/themes/sanitary-panels/templates/comic-nav.php
+++ wp-content/themes/sanitary-panels/templates/comic-nav.php
@@ -1,11 +1,23 @@
 <div class="col-sm-12 text-center">
 <nav class="sprites">
-  <a class="sprite-first">first</a>
-  <?php previous_post_link( '%link', 'previous', $in_same_term = true, $taxonomy = 'category' ); ?>
 
-  <a class="sprite-random" href="/random/">random</a>
-  <?php next_post_link( '%link', 'next', $in_same_term = true, $taxonomy = 'category' ); ?>
+  <a class="sprite-first"
+    href="<?php echo get_bloginfo('wpurl'); ?>/first/">
+  first</a>
+  <a class="sprite-prev"
+    href="<?php
+    $p = get_previous_post( true ); if ($p) echo get_permalink($p); else echo "#"; ?>">
+  previous</a>
+  <a class="sprite-random"
+    href="<?php echo get_bloginfo('wpurl'); ?>/random/">
+  random</a>
+  <a class="sprite-next"
+    href="<?php
+    $p = get_next_post( true ); if ($p) echo get_permalink($p); else echo "#"; ?>">
+  next</a>
+  <a class="sprite-last"
+    href="<?php echo get_bloginfo('wpurl'); ?>/last/">
+  last</a>
 
-  <a class="sprite-last">last</a>
 </nav>
 </div>
