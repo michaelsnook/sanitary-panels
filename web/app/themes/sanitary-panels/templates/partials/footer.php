1472e75f0fd0c7780c99e447ffc1bd1056ce4c2e
michaelsnook <michael.snook@gmail.com>
2018-09-24 17:45:42 +0530

    Remove plugin formerly used to implement /random route

--- web/app/themes/sanitary-panels/templates/partials/footer.php
+++ web/app/themes/sanitary-panels/templates/partials/footer.php
@@ -7,6 +7,6 @@
     <p>&copy; Sanitary Panels 2016 by Rachita Taneja.
       Licensed under a <a href="https://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 License</a>.
       Made with love by <a href="https://github.com/michaelsnook/sanitary-panels/">Michael Snook</a>.
-      Built on the <a href="https://roots.io/sage/">Sage theme for WordPress</a>.</p>
+      Built on Sage and Bedrock by <a href="https://roots.io">Roots</a>.</p>
   </div>
 </div>
