cb6145d241ea05ffdcef850fa1711e5c8e771612
root <michael.snook@gmail.com>
2019-03-04 13:09:43 +0000

    Add vars for WP Offload Site plugin

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,3 +5,10 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+
+/**
+ * S3 Keys
+ */
+define('AS3CF_AWS_ACCESS_KEY_ID', env('AS3CF_AWS_ACCESS_KEY_ID'));
+define('AS3CF_AWS_SECRET_ACCESS_KEY', env('AS3CF_AWS_SECRET_ACCESS_KEY'));
+
